791b3ae6b524b1bdc207341a52cea4740a28f870
Peter Schultz <schultz.peter@hotmail.com>
2013-10-02 11:45:27 +0200

    Use first address instead of last

--- files/default/plugins/vagrant-net.rb
+++ files/default/plugins/vagrant-net.rb
@@ -1,4 +1,3 @@
-
 # borrowed from https://gist.github.com/2050259
 
 provide "ipaddress"
@@ -6,5 +5,6 @@ require_plugin "#{os}::network"
 network["interfaces"]["eth1"]["addresses"].each do |ip, params|
   if params['family'] == ('inet')
     ipaddress ip
+    break
   end
 end
